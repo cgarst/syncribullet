3e51d682fb52065a74474d191a3fc5eebd32a7a6
Corey Garst <corey.garst@gmail.com>
2026-02-15 15:46:28 -0500

    mdblist catalogs

--- src/utils/receivers/mdblist/constants.ts
+++ src/utils/receivers/mdblist/constants.ts
@@ -3,7 +3,11 @@ import type {
   Importers,
 } from '~/utils/importer/types/importers';
 import type { ImportCatalogs } from '~/utils/importer/types/user-settings/import-catalogs';
-import { ManifestReceiverTypes } from '~/utils/manifest';
+import {
+  ManifestCatalogExtraParameters,
+  ManifestReceiverTypes,
+} from '~/utils/manifest';
+import type { ManifestCatalogItem } from '~/utils/manifest';
 import type { ReceiverInfo } from '~/utils/receiver/receiver';
 import { IDSources } from '~/utils/receiver/types/id';
 import { Receivers } from '~/utils/receiver/types/receivers';
@@ -32,17 +36,187 @@ export const syncIds = [
   [IDSources.TRAKT],
 ] as const satisfies Readonly<Readonly<IDSources[]>[]>;
 
-export const manifestCatalogItems = [] as const;
+export const manifestCatalogItems = [
+  {
+    id: 'syncribullet-mdblist-movies-plantowatch',
+    type: ManifestReceiverTypes.MOVIE,
+    name: 'MDBList - Plan to Watch',
+    extra: [
+      { name: ManifestCatalogExtraParameters.GENRE, isRequired: false },
+      { name: ManifestCatalogExtraParameters.SKIP, isRequired: false },
+    ],
+  },
+  {
+    id: 'syncribullet-mdblist-movies-completed',
+    type: ManifestReceiverTypes.MOVIE,
+    name: 'MDBList - Completed',
+    extra: [
+      { name: ManifestCatalogExtraParameters.GENRE, isRequired: false },
+      { name: ManifestCatalogExtraParameters.SKIP, isRequired: false },
+    ],
+  },
+  {
+    id: 'syncribullet-mdblist-shows-watching',
+    type: ManifestReceiverTypes.SERIES,
+    name: 'MDBList - Watching',
+    extra: [
+      { name: ManifestCatalogExtraParameters.GENRE, isRequired: false },
+      { name: ManifestCatalogExtraParameters.SKIP, isRequired: false },
+    ],
+  },
+  {
+    id: 'syncribullet-mdblist-shows-plantowatch',
+    type: ManifestReceiverTypes.SERIES,
+    name: 'MDBList - Plan to Watch',
+    extra: [
+      { name: ManifestCatalogExtraParameters.GENRE, isRequired: false },
+      { name: ManifestCatalogExtraParameters.SKIP, isRequired: false },
+    ],
+  },
+  {
+    id: 'syncribullet-mdblist-shows-completed',
+    type: ManifestReceiverTypes.SERIES,
+    name: 'MDBList - Completed',
+    extra: [
+      { name: ManifestCatalogExtraParameters.GENRE, isRequired: false },
+      { name: ManifestCatalogExtraParameters.SKIP, isRequired: false },
+    ],
+  },
+  {
+    id: 'syncribullet-mdblist-shows-dropped',
+    type: ManifestReceiverTypes.SERIES,
+    name: 'MDBList - Dropped',
+    extra: [
+      { name: ManifestCatalogExtraParameters.GENRE, isRequired: false },
+      { name: ManifestCatalogExtraParameters.SKIP, isRequired: false },
+    ],
+  },
+] as const satisfies Readonly<ManifestCatalogItem<MDBListMCIT>[]>;
 
 export const defaultCatalogs: Readonly<
   (typeof manifestCatalogItems)[number]['id'][]
-> = [] as const satisfies Readonly<(typeof manifestCatalogItems)[number]['id'][]>;
+> = [
+  'syncribullet-mdblist-movies-plantowatch',
+  'syncribullet-mdblist-movies-completed',
+  'syncribullet-mdblist-shows-watching',
+  'syncribullet-mdblist-shows-plantowatch',
+  'syncribullet-mdblist-shows-completed',
+] as const satisfies Readonly<(typeof manifestCatalogItems)[number]['id'][]>;
 
 export const defaultImportCatalogs: Readonly<
   Record<Importers, Readonly<ImportCatalogs<MDBListMCIT, ImporterMCITypes>[]>>
 > = {
   simkl: [],
-  stremio: [],
+  stremio: [
+    {
+      id: 'syncribullet-mdblist-movies-plantowatch',
+      value: true,
+      filters: {
+        moviesStateFlaggedWatched: false,
+        moviesStateFlaggedUnwatched: true,
+        moviesStateFlaggedDropped: false,
+        seriesStateFlaggedWatched: null,
+        seriesStateFlaggedUnwatched: null,
+        seriesStateFlaggedDropped: null,
+        seriesStateFlaggedOnHold: null,
+        seriesPreferStateFlaggedWatchedOverWatchCount: null,
+        seriesUseCinemetaComparison: null,
+        seriesStateHasWatchCount: null,
+        seriesBackfillEpisodes: null,
+        supportsTypes: [ManifestReceiverTypes.MOVIE],
+      },
+    },
+    {
+      id: 'syncribullet-mdblist-movies-completed',
+      value: true,
+      filters: {
+        moviesStateFlaggedWatched: true,
+        moviesStateFlaggedUnwatched: false,
+        moviesStateFlaggedDropped: false,
+        seriesStateFlaggedWatched: null,
+        seriesStateFlaggedUnwatched: null,
+        seriesStateFlaggedDropped: null,
+        seriesStateFlaggedOnHold: null,
+        seriesPreferStateFlaggedWatchedOverWatchCount: null,
+        seriesStateHasWatchCount: null,
+        seriesUseCinemetaComparison: null,
+        seriesBackfillEpisodes: null,
+        supportsTypes: [ManifestReceiverTypes.MOVIE],
+      },
+    },
+    {
+      id: 'syncribullet-mdblist-shows-watching',
+      value: true,
+      filters: {
+        moviesStateFlaggedWatched: null,
+        moviesStateFlaggedUnwatched: null,
+        moviesStateFlaggedDropped: null,
+        seriesStateFlaggedWatched: false,
+        seriesStateFlaggedUnwatched: true,
+        seriesStateFlaggedDropped: false,
+        seriesStateFlaggedOnHold: false,
+        seriesStateHasWatchCount: true,
+        seriesPreferStateFlaggedWatchedOverWatchCount: true,
+        seriesUseCinemetaComparison: true,
+        seriesBackfillEpisodes: true,
+        supportsTypes: [ManifestReceiverTypes.SERIES],
+      },
+    },
+    {
+      id: 'syncribullet-mdblist-shows-plantowatch',
+      value: true,
+      filters: {
+        moviesStateFlaggedWatched: null,
+        moviesStateFlaggedUnwatched: null,
+        moviesStateFlaggedDropped: null,
+        seriesStateFlaggedWatched: false,
+        seriesStateFlaggedUnwatched: true,
+        seriesStateFlaggedDropped: false,
+        seriesStateFlaggedOnHold: false,
+        seriesStateHasWatchCount: false,
+        seriesPreferStateFlaggedWatchedOverWatchCount: true,
+        seriesUseCinemetaComparison: false,
+        seriesBackfillEpisodes: false,
+        supportsTypes: [ManifestReceiverTypes.SERIES],
+      },
+    },
+    {
+      id: 'syncribullet-mdblist-shows-completed',
+      value: true,
+      filters: {
+        moviesStateFlaggedWatched: null,
+        moviesStateFlaggedUnwatched: null,
+        moviesStateFlaggedDropped: null,
+        seriesStateFlaggedWatched: true,
+        seriesStateFlaggedUnwatched: false,
+        seriesStateFlaggedDropped: false,
+        seriesStateFlaggedOnHold: false,
+        seriesStateHasWatchCount: null,
+        seriesPreferStateFlaggedWatchedOverWatchCount: true,
+        seriesUseCinemetaComparison: false,
+        seriesBackfillEpisodes: true,
+        supportsTypes: [ManifestReceiverTypes.SERIES],
+      },
+    },
+    {
+      id: 'syncribullet-mdblist-shows-dropped',
+      value: true,
+      filters: {
+        moviesStateFlaggedWatched: null,
+        moviesStateFlaggedUnwatched: null,
+        moviesStateFlaggedDropped: null,
+        seriesStateFlaggedWatched: false,
+        seriesStateFlaggedUnwatched: true,
+        seriesStateFlaggedDropped: true,
+        seriesStateFlaggedOnHold: false,
+        seriesStateHasWatchCount: true,
+        seriesPreferStateFlaggedWatchedOverWatchCount: true,
+        seriesUseCinemetaComparison: true,
+        seriesBackfillEpisodes: true,
+        supportsTypes: [ManifestReceiverTypes.SERIES],
+      },
+    },
+  ],
 } as const satisfies Readonly<
   Record<Importers, Readonly<ImportCatalogs<MDBListMCIT, ImporterMCITypes>[]>>
 >;
--- src/utils/receivers/mdblist/types/catalog/catalog-status.ts
+++ src/utils/receivers/mdblist/types/catalog/catalog-status.ts
@@ -2,4 +2,5 @@ export enum MDBListCatalogStatus {
   WATCHING = 'watching',
   COMPLETED = 'completed',
   PLANTOWATCH = 'plantowatch',
+  DROPPED = 'dropped',
 }
